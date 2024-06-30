4ac51b166f40b10b328e5a96b641e6432aa2bea2
cold1840 <2014884355@qq.com>
2024-06-30 10:13:48 +0800

    save system ready

--- SporeBG_Engine/base.py
+++ SporeBG_Engine/base.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+import json # for saving games
 
 GROW=1
 MOVE=2
@@ -42,6 +43,10 @@ class GameE():#GameEngine 负责游戏规则和存档处理
 		self.germs_u=[]
 		self.player_now=1
 		self.germs_left=[]
+		self.history={'InitField':self.field,'InitGShape':GShape,\
+			'InitPlayer':1,\
+			'AllSteps':[]}
+		self.step_step=[]
 		self.roll()
 	def fieldset(self,pos,aim):
 		x,y=pos
@@ -85,9 +90,14 @@ class GameE():#GameEngine 负责游戏规则和存档处理
 			return self.germs_u
 		return self.germs_e
 	def roll(self):
+		if self.step_step!=[]:
+			self.history['AllSteps'].append(self.step_step)
+		self.step_step=[]
+		print(self.history)
 		self.germs_pick()
 		self.player_now=2 if self.player_now==1 else 1
 		self.germs_left=self.germs_now().copy()
+		#self.save_as_file('save.json')
 	def show(self):
 		for i in self.field:
 			print(i)
@@ -112,9 +122,23 @@ class GameE():#GameEngine 负责游戏规则和存档处理
 				self.fieldset(f_pos,0)
 			self.fieldset(t_pos,self.player_now)
 			del self.germs_left[germs_inclu(f_pos,self.germs_left)[0]]
+			self.step_step.append([mode,f_pos,t_pos])
 			if len(self.germs_left)==0:
 				self.roll()
 		return check
+	def go_step(self,step):
+		mode,f_pos,t_pos=step
+		return self.go(mode,f_pos,t_pos)
+	def go_steps(self,steps):
+		field_copy=self.field # 备份棋盘
+		o=0
+		for step in steps:
+			check = self.go_step(step)
+			if not check[0]:
+				self.field=field_copy # 还原棋盘
+				return False,o,check
+			o+=1
+		return True,o,200
 	def allow_pick_base(self,germ,aim=0):
 		allow=[]
 		g=germ.copy()
@@ -148,7 +172,29 @@ class GameE():#GameEngine 负责游戏规则和存档处理
 			elif mode==SPORE:
 				return self.allow_pick_spore(f_pos)
 		return []
-
+	def save_as_file(self,filename):#将存档写入json文件
+		try:
+			with open(filename,'w') as f:
+				json.dump(self.history,f)
+			return True,200
+		except BaseException as e:
+			return False,e
+	def load_fr_history(self,history):#从合法的history字典中加载游戏
+		self.W,self.H=history['InitGShape']
+		self.field=history['InitField']
+		self.player_now=history['InitPlayer']
+		self.history=history
+		self.step_step=[]
+		self.roll()
+		for i in history['AllSteps']:
+			go_steps(i)
+	def load_fr_file(self,filename):#从json文件中加载存档
+		try:
+			with open(filename,'r') as f:
+				history=json.loads(f.read())
+			return self.load_fr_history(history)
+		except BaseException as e:
+			return False,e
 class GameEG(GameE):#GameEngineGraph 扩展原始的图形计算工具
 	def __init__(self,GShape:Tuple[int,int],ScrRect:Tuple[int,int,int,int]):
 		GameE.__init__(self,GShape)
@@ -172,11 +218,21 @@ def main():
 	print('player_now',g.player_now)
 	print('germs_left',g.germs_left)
 	print('check',g.check(GROW,(6,6),(5,6)))
-	g.go(GROW,(6,6),(5,6))
+	g.go_steps([[1, (6, 6), (5, 6)], [1, (5, 5), (6, 5)]])
+	g.show()
+	g.go(GROW,(0,0),(0,1))
 	g.show()
 	print('germs_left',g.germs_left)
+	g.go(GROW,(2,0),(2,1))
+	g.go(SPORE,(5,6),(3,6))
+	g.show()
+	g.save_as_file('save.json')
+	print('player_now',g.player_now)
 
 
 
 if __name__=='__main__':
-	main()
+	g=GameE((7,7))
+	print(g.load_fr_file('save.json'))
+	g.show()
+	#main()
